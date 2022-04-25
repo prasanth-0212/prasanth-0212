b9b0038a17bbe96ecf4e27cafac7e6ba38b2627c
prasanth-0212 <100260139+prasanth-0212@users.noreply.github.com>
2022-04-25 15:39:50 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,18 @@
+
+public class Pattern2 {
+	public static void main(String[] args) {
+		int k=1;
+		 int j;
+		 for (int i = 0; i < 5; i++) {
+			
+			for (j = 1; j <=k; ) {		
+					System.out.print(j);							
+			}
+			j++;
+			k++;
+			System.out.println();
+			
+		}
+	}
+
+}
